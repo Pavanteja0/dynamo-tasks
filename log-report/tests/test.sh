b5ce0106308037988f5abdd6b40ad801b744bf0a
Pavanteja0 <yendurikalyan2005@gmail.com>
2026-07-26 17:59:20 +0000

    fix: write clean scripts

--- log-report/tests/test.sh
+++ log-report/tests/test.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 set +e
 mkdir -p /logs/verifier
-
 python -m pytest /tests/test_outputs.py --json-ctrf /logs/verifier/ctrf.json
 RESULT=$?
-
 if [ $RESULT -eq 0 ]; then
   echo "1" > /logs/verifier/reward.txt
 else
   echo "0" > /logs/verifier/reward.txt
 fi
-
 exit $RESULT
